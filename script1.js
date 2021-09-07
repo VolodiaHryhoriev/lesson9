ae496e7acbc54a8450329b94aff7e2eb08f75e1f
VolodiaHryhoriev <88433088+VolodiaHryhoriev@users.noreply.github.com>
2021-09-07 20:56:16 +0300

    second commit

--- script1.js
+++ script1.js
@@ -1,46 +1,24 @@
 let writeTask = document.querySelector('.writeTask')
 let pageLimit = 5;
-let counter = 1
+let counter = 1;
+let parentDiv
 
 document.getElementById('add').addEventListener('click', () => {
-if (writeTask.value !== '') {
-for (let i = 1; i < pageLimit; i++ ) {
-let parentDiv = document.querySelector('.list');
-let newDiv = document.createElement('div');
 
+    parentDiv = document.querySelector('.list');
+    let newDiv = document.createElement('div');
+    newDiv.innerHTML = `<input type="button" onclick="removeTask(event)" class="remove${counter}"><span class="remove${counter}">${writeTask.value}</span>`;
+    parentDiv.appendChild(newDiv);
+    writeTask.value = '';
+    counter++
 
-newDiv.innerHTML = `<input type="button" id="remove${counter}"><span id="removeSpan${counter}">${writeTask.value}</span>`;
-parentDiv.appendChild(newDiv);
+});
 
-writeTask.value = '';
-counter++
-document.getElementById('remove1').addEventListener('click', () => {
-    if (parentDiv.children !== 'remove1') {
-     document.getElementById(`remove1`).remove()
-     document.getElementById(`removeSpan1`).remove() 
+function removeTask(event) {
+    if (parentDiv.children.length === 1) {
+        alert('Останній таск зі списку Ви не можете видалити');
     } else {
-        alert('Останній таск зі списку Ви не можете видалити')
-    } 
-})
-document.getElementById('remove2').addEventListener('click', () => {
-    document.getElementById(`remove2`).remove()
-    document.getElementById(`removeSpan2`).remove()  
-})
-document.getElementById('remove3').addEventListener('click', () => {
-    document.getElementById(`remove3`).remove()
-    document.getElementById(`removeSpan3`).remove()  
-})
-document.getElementById('remove4').addEventListener('click', () => {
-    document.getElementById(`remove4`).remove()
-    document.getElementById(`removeSpan4`).remove()  
-})
-document.getElementById('remove5').addEventListener('click', () => {
-    document.getElementById(`remove5`).remove()
-    document.getElementById(`removeSpan5`).remove()  
-})
-
-
+        event.target.parentElement.remove();
+    }
+    console.log(parentDiv.children.length)
 }
-} else {
-    alert('Пусте поле не можна добавити');
-}})
